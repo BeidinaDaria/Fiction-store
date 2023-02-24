40501ec4acd392d3a7fb66ed17c54c504e3e95f8
YBarsukova <115146802+YBarsukova@users.noreply.github.com>
2023-02-24 14:18:53 +0300

    Add files via upload
    Добавлен класс Item, элементы которого будут выводиться на главной странице

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-from flask import Flask, render_template, request
 
-from sqlalchemy import create_engine
+from flask import Flask, render_template, request, url_for, redirect
+
+from flask_sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from DataBase.database_interaction import Items, Base
+import os
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 
-
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Item(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable = False)
+    price = db.Column(db.Double, nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    def __repr__(self):
+        return self.title
+
 
 @app.route('/')
 def index():
+    items=Item.query.order_by(Item.price).all()
     return render_template('index.html')
 
 
@@ -31,8 +49,10 @@ def basket():
 def product():
     return render_template('product.html')
 
-
-#   entry point
+    
+    
+    
+    #   entry point
 if __name__ == "__main__":
     # database config and session creating
     engine = create_engine("sqlite:///fiction_store.db")
