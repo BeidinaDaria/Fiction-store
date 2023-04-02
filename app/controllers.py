c022cf9b1506b038880f3c92e598925de5fec83f
YBarsukova <115146802+YBarsukova@users.noreply.github.com>
2023-04-02 17:00:15 +0300

    Update controllers

--- app/controllers.py
+++ app/controllers.py
@@ -6,7 +6,7 @@ from . import models
 #   APP ROUTING
 @app.route('/')
 def index():
-    return render_template('index')
+    return render_template('index.html')
 
 @app.route('/product', methods=['GET', 'POST'])
 def search():
@@ -17,19 +17,19 @@ def search():
 
 @app.route('/catalog')
 def about():
-    return render_template('catalog')
+    return render_template('catalog.html')
 
 
 @app.route('/favourites')
 def favourites():
-    return render_template('favourites')
+    return render_template('favourites.html')
 
 
 @app.route('/basket')
 def basket():
-    return render_template('basket')
+    return render_template('basket.html')
 
 
 # @app.route('/product')
 # def product():
-#     return render_template('product')
+#     return render_template('product.html')
