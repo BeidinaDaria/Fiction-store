1b1464d787111a80932adfeb7ba15e06cc461fff
YBarsukova <115146802+YBarsukova@users.noreply.github.com>
2023-04-21 14:33:43 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -14,3 +14,11 @@ class Items(db.Model):
     colors      = db.Column(db.JSON)
     science     = db.Column(db.String(50))
     comments    = db.Column(db.JSON)
+class User(db.Model):
+    __tablename__ = "users"
+    id          = db.Column(db.Integer, primary_key=True)
+    login       = db.Column(db.String(100), nullable=False)
+    password    = db.Column(db.String(100), nullable=False)
+    name        = db.Column(db.String(100), nullable=False)
+    surname     = db.Column(db.String(100), nullable=False)
+    comments    = db.Column(db.JSON)
