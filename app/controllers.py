5ee19355c7515558c3f248563d2eebf02bc77ccc
RIM <Kirillll.2001@yandex.ru>
2023-04-27 16:36:08 +0300

    + Basket and favourites models and display

--- app/controllers.py
+++ app/controllers.py
@@ -1,43 +1,57 @@
 from app import app, db
-from .models import Items, Users
+from .models import Item, User
 from flask import render_template, request, redirect, make_response
 
 #   APP ROUTING
 
 @app.route('/', methods=['GET'])
 def index():
-    items = db.session.query(Items).limit(5).all()
+    items = db.session.query(Item).limit(5).all()
     # TODO: Пока что сортировка по цене, а не рейтингу в комментариях
-    best_items = db.session.query(Items).order_by(Items.price).limit(5).all()
-    return render_template('index.html', items=items, best_items=best_items)
+    best_items = db.session.query(Item).order_by(Item.price).limit(5).all()
+
+    return render_template('index.html', favs_len=10, basket_len=10, items=items, best_items=best_items)
 
 
 @app.route('/product', methods=['GET'])
 def product():
     req = request.args.get('id')
     if req:
-        item = db.session.query(Items).filter(Items.id == req).first()
-        return render_template('product.html', item=item)
+        item = db.session.query(Item).filter(Item.id == req).first()
+        if item:
+            return render_template('product.html', favs_len=10, basket_len=10, item=item)
     return "Page not found", 404
 
 
 @app.route('/catalog', methods=['GET'])
 def catalog():
     req = request.args.get('search')
-    items = db.session.query(Items).filter(Items.title == req[0].upper()+req[1:].lower()).all() if req \
-        else db.session.query(Items).all()
+    items = db.session.query(Item).filter(Item.title == req[0].upper()+req[1:].lower()).all() if req \
+        else db.session.query(Item).all()
     # TODO: Добавить вёрстку в случае, когда по запросу ничего не найдено
-    return render_template('catalog.html', items=items)
+    return render_template('catalog.html', favs_len=10, basket_len=10, items=items)
 
 
 @app.route('/favourites')
 def favourites():
-    return render_template('favourites.html')
+    # check if not login
+    token = request.cookies.get('token')
+    if not token:
+        return render_template('favourites.html', favs_len=10, basket_len=10)
+    # find user in DB
+    favourites = db.session.query(User).filter_by(token=token).first().favourites
+    return render_template('favourites.html', favs_len=10, basket_len=10, favourites=favourites)
 
 
 @app.route('/basket')
 def basket():
-    return render_template('basket.html')
+    # check if not login
+    token = request.cookies.get('token')
+    if not token:
+        return render_template('basket.html', favs_len=10, basket_len=10)
+    # find user in DB
+    basket = db.session.query(User).filter_by(token=token).first().basket
+    return render_template('basket.html', favs_len=10, basket_len=10, basket=basket)
 
 
 @app.route('/profile', methods=['GET'])
@@ -47,10 +61,10 @@ def profile():
     if not token:
         return redirect('/login')
     # find user in DB
-    user = db.session.query(Users).filter_by(token=token).first()
+    user = db.session.query(User).filter_by(token=token).first()
     if not user:
         return redirect('/login')
-    return render_template('profile.html', user=user)
+    return render_template('profile.html', favs_len=10, basket_len=10, user=user)
 
 
 # JWT Token Authentication
@@ -58,18 +72,18 @@ def profile():
 def login():
     # check if already login
     token = request.cookies.get('token')
-    if token and db.session.query(Users).filter_by(token=token).first():
+    if token and db.session.query(User).filter_by(token=token).first():
         return redirect('/profile')
     # get data from the form
     email = request.form.get('email')
     password = request.form.get('password')
     if not email or not password:
-        return render_template('login.html')
+        return render_template('login.html', favs_len=10, basket_len=10)
     # find user in DB
-    user = db.session.query(Users).filter_by(email=email, password=password).first()
+    user = db.session.query(User).filter_by(email=email, password=password).first()
     # set token in cookie
     if user:
         response = make_response(redirect('/profile'))
         response.set_cookie('token', user.token)
         return response
-    return render_template('login.html', message="Неправильный логин или пароль")
+    return render_template('login.html', favs_len=10, basket_len=10, message="Неправильный логин или пароль")
--- app/models.py
+++ app/models.py
@@ -1,37 +1,50 @@
 from app import db
 
+#   MODEL OF BASKET AND FAVOURITES TABLES
+
+basket = db.Table("basket",
+            db.Column("user_id", db.Integer, db.ForeignKey('user.id')),
+            db.Column("item_id", db.Integer, db.ForeignKey('item.id'))
+)
+favourites = db.Table("favourites",
+            db.Column("user_id", db.Integer, db.ForeignKey('user.id')),
+            db.Column("item_id", db.Integer, db.ForeignKey('item.id'))
+)
+
 #   MODEL OF ITEMS TABLE
 
-class Items(db.Model):
-    __tablename__ = "items"
-    id          = db.Column(db.Integer, primary_key=True)
-    title       = db.Column(db.String(100), nullable=False)
-    price       = db.Column(db.Float, nullable=False)
-    description = db.Column(db.String(1000), nullable=False)
-    images      = db.Column(db.JSON) # [ "str", ... ]
-    colors      = db.Column(db.JSON) # [ "str", ... ]
-    science     = db.Column(db.String(50))
-    comments    = db.relationship("Comments", backref="item")
+class Item(db.Model):
+    __tablename__ = "item"
+    id              = db.Column(db.Integer, primary_key=True)
+    title           = db.Column(db.String(100), nullable=False)
+    price           = db.Column(db.Float, nullable=False)
+    description     = db.Column(db.String(1000), nullable=False)
+    images          = db.Column(db.JSON) # [ "str", ... ]
+    colors          = db.Column(db.JSON) # [ "str", ... ]
+    science         = db.Column(db.String(50))
+    comments        = db.relationship("Comment", backref="item")
 
 #   MODEL OF USERS TABLE
 
-class Users(db.Model):
-    __tablename__ = "users"
+class User(db.Model):
+    __tablename__ = "user"
     id          = db.Column(db.Integer, primary_key=True)
     email       = db.Column(db.String(100), nullable=False, unique=True)
     password    = db.Column(db.String(100), nullable=False)
-    token       = db.Column(db.String(100))
+    token       = db.Column(db.String(100), nullable=False, unique=True)
     name        = db.Column(db.String(50), nullable=False)
     surname     = db.Column(db.String(50))
-    comments    = db.relationship("Comments", backref="user")
+    comments    = db.relationship("Comment", backref="user")
+    basket      = db.relationship("Item", secondary=basket, backref=db.backref('users_have_basket'))
+    favourites  = db.relationship("Item", secondary=favourites, backref=db.backref('users_have_fav'))
 
 #   MODEL OF COMMENTS TABLE
 
-class Comments(db.Model):
-    __tablename__ = "comments"
+class Comment(db.Model):
+    __tablename__ = "comment"
     id          = db.Column(db.Integer, primary_key=True)
-    user_id     = db.Column(db.Integer, db.ForeignKey('users.id'))
-    item_id     = db.Column(db.Integer, db.ForeignKey('items.id'))
+    user_id     = db.Column(db.Integer, db.ForeignKey('user.id'))
+    item_id     = db.Column(db.Integer, db.ForeignKey('item.id'))
     date        = db.Column(db.String(10), nullable=False) # "day.month.year"
     score       = db.Column(db.Integer, nullable=False)
     pros        = db.Column(db.String(400))
