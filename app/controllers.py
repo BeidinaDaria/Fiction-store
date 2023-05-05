f0a975a85b4753ab916667bc298c208a7420d273
RIM <Kirillll.2001@yandex.ru>
2023-05-06 02:07:09 +0300

    + Signup

--- app/controllers.py
+++ app/controllers.py
@@ -1,3 +1,4 @@
+import uuid
 from app import app, db
 from .models import Item, User
 from flask import render_template, request, redirect, make_response, Response
@@ -6,17 +7,15 @@ from flask import render_template, request, redirect, make_response, Response
 
 def authToken(request):
     token = request.cookies.get('token')
-    if not token or not (user := db.session.query(User).filter_by(token=token).first()):
-        return False
-    else:
+    if token and (user := db.session.query(User).filter_by(token=token).first()):
         return user
+    return False
 
 def favs_basket_len(user):
     if user:
         return len(user.favourites), len(user.basket)
-    else:
-        # TODO: get from cookies
-        return 0, 0
+    # TODO: get from cookies
+    return 0, 0
 
 
 @app.route('/', methods=['GET'])
@@ -41,7 +40,7 @@ def product():
         in_basket = user.basket.count(item) != 0
         favs_len, basket_len = favs_basket_len(user)
     else:
-        # get from cookies
+        # TODO: get from cookies
         in_fav = 0
         in_basket = 0
         favs_len, basket_len = favs_basket_len(False)
@@ -68,7 +67,7 @@ def favourites():
     if user:
         favourites=user.favourites
     else:
-        # get from cookies
+        # TODO: get from cookies
         favourites=None
     return render_template('favourites.html', favs_len=favs_len,
                            basket_len=basket_len, favourites=favourites)
@@ -81,7 +80,7 @@ def basket():
     if user:
         basket=user.basket
     else:
-        # get from cookies
+        # TODO: get from cookies
         basket=None
     return render_template('basket.html', favs_len=favs_len,
                            basket_len=basket_len, basket=basket)
@@ -90,8 +89,7 @@ def basket():
 @app.route('/profile', methods=['GET'])
 def profile():
     # check if not login
-    user = authToken(request)
-    if not user:
+    if not (user := authToken(request)):
         return redirect('/login')
     favs_len, basket_len = favs_basket_len(user)
     return render_template('profile.html', favs_len=favs_len,
@@ -102,29 +100,74 @@ def profile():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     # check if already login
-    user = authToken(request)
-    if user:
+    if (user := authToken(request)):
         return redirect('/profile')
     favs_len, basket_len = favs_basket_len(False)
-    # get data from the form
+    # get form data
     email = request.form.get('email')
-    password = request.form.get('password')
+    password = request.form.get('pwd')
     if not email or not password:
-        return render_template('login.html', favs_len=favs_len,
-                               basket_len=basket_len)
-    # find user in DB & set token in cookie
-    if (user := db.session.query(User).filter_by(email=email, password=password).first()):
-        response = make_response(redirect('/profile'))
-        response.set_cookie('token', user.token)
-        return response
-    return render_template('login.html', favs_len=favs_len,
-                           basket_len=basket_len,
+        return render_template('login.html', favs_len=favs_len, basket_len=basket_len)
+    if not (user := db.session.query(User).filter_by(email=email, password=password).first()):
+        return render_template('login.html', favs_len=favs_len, basket_len=basket_len,
                            message="Неправильный логин или пароль")
+    # generate unique token, find user in DB & set token in cookie
+    response = make_response(redirect('/profile'))
+    while True:
+        try:
+            user.token = uuid.uuid4().hex
+            db.session.commit()
+        except Exception as e:
+            print(e)
+        else:
+            response.set_cookie('token', user.token)
+            return response
 
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template('registration.html')
+    # check if already login
+    if authToken(request):
+        return redirect('/profile')
+    favs_len, basket_len = favs_basket_len(False)
+    # get form data
+    email = request.form.get('email')
+    username = request.form.get('username')
+    password = request.form.get('pwd')
+    if not email or not username or not password:
+        return render_template('registration.html',
+                               favs_len=favs_len, basket_len=basket_len)
+    if password != request.form.get('pwd-repeat'):
+        return render_template('registration.html', message="Введённые пароли не совпадают",
+                               favs_len=favs_len, basket_len=basket_len)
+    if len(password) < 6:
+        return render_template('registration.html', message="Пароль должен быть длинней 6 символов",
+                               favs_len=favs_len, basket_len=basket_len)
+    if db.session.query(User).filter_by(email=email).first():
+        return render_template('registration.html', message="Пользователь с такой почтой уже зарегестрирован",
+                               favs_len=favs_len, basket_len=basket_len)
+    # generate unique token, add user to DB & set token in cookie
+    response = make_response(redirect('/'))
+    while True:
+        try:
+            token = uuid.uuid4().hex
+            db.session.add(User(
+                email=email, name=username,
+                token=token, password=password
+            ))
+            db.session.commit()
+        except Exception as e:
+            print(e)
+        else:
+            response.set_cookie('token', token)
+            return response
+
+
+@app.route('/logout', methods=['POST'])
+def logout():
+    response = make_response(redirect('/'))
+    response.delete_cookie('token')
+    return response
 
 
 @app.errorhandler(404)
@@ -143,9 +186,10 @@ def basket_favourites(request, attr_name, method_name):
     method = getattr(getattr(user, attr_name), method_name)
     try:
         method(db.session.query(Item).filter_by(id=int(request.data)).first())
-    except ValueError:
+        db.session.commit()
+    except Exception as e:
+        print(e)
         return Response(status=412)
-    db.session.commit()
     return Response(status=200)
 
 @app.route('/favourites/add', methods=['POST'])
