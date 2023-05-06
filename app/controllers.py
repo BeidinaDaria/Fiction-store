f131d7b2508df7965991c0e2a82ceb0d41256d99
RIM <Kirillll.2001@yandex.ru>
2023-05-06 17:49:18 +0300

    + Favourites and basket without login

--- app/controllers.py
+++ app/controllers.py
@@ -11,11 +11,10 @@ def authToken(request):
         return user
     return False
 
-def favs_basket_len(user):
+def favs_basket_len(user, cookies):
     if user:
         return len(user.favourites), len(user.basket)
-    # TODO: get from cookies
-    return 0, 0
+    return len(cookies.get('favourites') or "")//2, len(cookies.get('basket') or "")//2
 
 
 @app.route('/', methods=['GET'])
@@ -23,7 +22,7 @@ def index():
     items = db.session.query(Item).limit(5).all()
     # TODO: Пока что сортировка по цене, а не рейтингу в комментариях
     best_items = db.session.query(Item).order_by(Item.price).limit(5).all()
-    favs_len, basket_len = favs_basket_len(authToken(request))
+    favs_len, basket_len = favs_basket_len(authToken(request), request.cookies)
     return render_template('index.html', favs_len=favs_len,
                            basket_len=basket_len, items=items,
                            best_items=best_items)
@@ -31,19 +30,17 @@ def index():
 
 @app.route('/product', methods=['GET'])
 def product():
-    req = request.args.get('id')
-    if not req or not (item := db.session.query(Item).filter(Item.id == req).first()):
+    id = request.args.get('id')
+    if not id or not (item := db.session.query(Item).filter(Item.id == id).first()):
         return redirect("404", code=404), {"Refresh": "0; url=/404"}
     user = authToken(request)
+    favs_len, basket_len = favs_basket_len(user, request.cookies)
     if user:
         in_fav = user.favourites.count(item) != 0
         in_basket = user.basket.count(item) != 0
-        favs_len, basket_len = favs_basket_len(user)
     else:
-        # TODO: get from cookies
-        in_fav = 0
-        in_basket = 0
-        favs_len, basket_len = favs_basket_len(False)
+        in_fav = (favourites := request.cookies.get('favourites')) and id+',' in favourites
+        in_basket = (basket := request.cookies.get('basket')) and id+',' in basket
     return render_template('product.html', favs_len=favs_len,
                            basket_len=basket_len, item=item,
                            in_fav=in_fav, in_basket=in_basket)
@@ -51,11 +48,10 @@ def product():
 
 @app.route('/catalog', methods=['GET'])
 def catalog():
-    req = request.args.get('search')
-    items = db.session.query(Item).filter(Item.title == req[0].upper()+req[1:].lower()).all() \
-            if req else db.session.query(Item).all()
-    favs_len, basket_len = favs_basket_len(authToken(request))
-    # TODO: Добавить вёрстку в случае, когда по запросу ничего не найдено
+    search = request.args.get('search')
+    items = db.session.query(Item).filter(Item.title == search[0].upper()+search[1:].lower()).all() \
+            if search else db.session.query(Item).all()
+    favs_len, basket_len = favs_basket_len(authToken(request), request.cookies)
     return render_template('catalog.html', favs_len=favs_len,
                            basket_len=basket_len, items=items)
 
@@ -63,12 +59,13 @@ def catalog():
 @app.route('/favourites')
 def favourites():
     user = authToken(request)
-    favs_len, basket_len = favs_basket_len(user)
+    favs_len, basket_len = favs_basket_len(user, request.cookies)
     if user:
-        favourites=user.favourites
+        favourites = user.favourites
+    elif (ids := request.cookies.get('favourites')):
+        favourites = db.session.query(Item).filter(Item.id.in_(ids.split(',')[:-1])).all()
     else:
-        # TODO: get from cookies
-        favourites=None
+        favourites = 0
     return render_template('favourites.html', favs_len=favs_len,
                            basket_len=basket_len, favourites=favourites)
 
@@ -76,12 +73,13 @@ def favourites():
 @app.route('/basket')
 def basket():
     user = authToken(request)
-    favs_len, basket_len = favs_basket_len(user)
+    favs_len, basket_len = favs_basket_len(user, request.cookies)
     if user:
-        basket=user.basket
+        basket = user.basket
+    elif (ids := request.cookies.get('basket')):
+        basket = db.session.query(Item).filter(Item.id.in_(ids.split(',')[:-1])).all()
     else:
-        # TODO: get from cookies
-        basket=None
+        basket = 0
     return render_template('basket.html', favs_len=favs_len,
                            basket_len=basket_len, basket=basket)
 
@@ -91,18 +89,18 @@ def profile():
     # check if not login
     if not (user := authToken(request)):
         return redirect('/login')
-    favs_len, basket_len = favs_basket_len(user)
+    favs_len, basket_len = favs_basket_len(user, False)
     return render_template('profile.html', favs_len=favs_len,
                            basket_len=basket_len, user=user)
 
 
-# JWT Token Authentication
+# Token Authentication
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     # check if already login
     if (user := authToken(request)):
         return redirect('/profile')
-    favs_len, basket_len = favs_basket_len(False)
+    favs_len, basket_len = favs_basket_len(False, request.cookies)
     # get form data
     email = request.form.get('email')
     password = request.form.get('pwd')
@@ -129,7 +127,7 @@ def signup():
     # check if already login
     if authToken(request):
         return redirect('/profile')
-    favs_len, basket_len = favs_basket_len(False)
+    favs_len, basket_len = favs_basket_len(False, request.cookies)
     # get form data
     email = request.form.get('email')
     username = request.form.get('username')
@@ -178,11 +176,25 @@ def page_not_found(e):
 #   AJAX METHODS
 
 def basket_favourites(request, attr_name, method_name):
+    # check for item id correctness
     if not request.data.isdigit():
         return Response(status=400)
     if not (user := authToken(request)):
-        # TODO: set in/get from cookie
-        return Response(status=200)
+        # if not login
+        attr_cookie = request.cookies.get(attr_name)
+        request_data = str(request.data, 'utf-8')+','
+        response = Response(status=200)
+        if method_name == "append":
+            # if already setted in cookie we don't need to set twice
+            if attr_cookie and request_data in attr_cookie:
+                return Response(status=400)
+            response.set_cookie(attr_name, (attr_cookie or "")+request_data)
+        else:
+            # if not setted in cookie we can't remove it from there
+            if not attr_cookie or not (request_data in attr_cookie):
+                return Response(status=400)
+            response.set_cookie(attr_name, attr_cookie.replace(request_data, ''))
+        return response
     method = getattr(getattr(user, attr_name), method_name)
     try:
         method(db.session.query(Item).filter_by(id=int(request.data)).first())
